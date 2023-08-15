ff957dc21e78de03f26bfc119c12377aa62dc175
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-15 19:20:15 +0300

    utils.py updated

---
+++ src/user/utils.py
@@ -0,0 +1,12 @@
+"""src/user/utils.py"""
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_async_session
+from src.user.models import User
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
