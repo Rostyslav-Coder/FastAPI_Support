473fd0057f3676ddaa6f6a19f150e834dfbe3eb0
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-15 19:30:04 +0300

    main.py updated

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@
 
 from fastapi import FastAPI
 
+from src.user.base_config import auth_backend, fastapi_users
+from src.user.routers import router as user_router
+from src.user.schemas import UserCreate, UserRead, UserUpdate
+
 app = FastAPI(title="Support App")
 
 
@@ -9,3 +13,30 @@ app = FastAPI(title="Support App")
 def root():
     """Welcome Page"""
     return {"message": "Welcome"}
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+app.include_router(user_router, prefix="/user", tags=["auth"])
