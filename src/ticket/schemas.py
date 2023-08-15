9feb83a02b1ac879e8d2aeeea82316b5bd41d0c0
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-15 23:50:35 +0300

    src/ticket/schemas.py updated

---
+++ src/ticket/schemas.py
@@ -0,0 +1,42 @@
+"""src/ticket/schemas.py"""
+
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+from src.ticket.models import TicketStatus
+
+
+class TicketIn(BaseModel):
+    """Ticket Data Input Model"""
+
+    title: str
+    text: str
+
+
+class TicketOut(TicketIn):
+    """Ticket Data Output Model"""
+
+    id: int
+    status: TicketStatus
+    user_id: int
+    manager_id: Optional[int]
+
+    __config__ = ConfigDict(from_attributes=True)
+
+
+class MessageIn(BaseModel):
+    """Message Data Input Model"""
+
+    text: str
+    user_id: int
+    ticket_id: int
+
+
+class MessageOut(MessageIn):
+    """Message Data Output Model"""
+
+    timestamp: datetime
+
+    __config__ = ConfigDict(from_attributes=True)
