399c7d58c842a690cecaddf06867b2abab617ae7
Rostyslav <rostyslavputnikov@gmail.com>
2023-08-18 13:19:59 +0300

    src/main.py updated

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 
-from src.ticket.routers.user_routers import router as ticket_router
+from src.ticket.routers import router as ticket_router
 from src.user.base_config import auth_backend, fastapi_users
 from src.user.routers import router as user_router
 from src.user.schemas import UserCreate, UserRead, UserUpdate
@@ -41,4 +41,4 @@ app.include_router(
 )
 
 app.include_router(user_router, prefix="/user", tags=["auth"])
-app.include_router(ticket_router, prefix="/user_ticket", tags=["user_ticket"])
+app.include_router(ticket_router, prefix="/ticket", tags=["ticket"])
